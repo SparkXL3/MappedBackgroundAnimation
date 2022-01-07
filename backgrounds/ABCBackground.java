7c7061b0f7aa758292aa7490d60de3e7f8cf4b72
gjwehnes <gjwehnes>
2022-01-06 18:47:37 -0700

    removed all starter code from classes to be modified in assignment

--- backgrounds/ABCBackground.java
+++ backgrounds/ABCBackground.java
@@ -1,27 +1,5 @@
-
-import java.awt.Image;
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.lang.Math;
-
-import javax.imageio.ImageIO;
 
 public class ABCBackground implements Background {
 
-
-import java.awt.Image;
-import java.awt.Rectangle;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-
-import javax.imageio.ImageIO;
-
-public class ABCBackground implements Background {
-
-    protected static int TILE_WIDTH = 50;
-    protected static int TILE_HEIGHT = 50;
-		
 }
--- sprites/ABCSprite.java
+++ sprites/ABCSprite.java
@@ -4,5 +4,5 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class ABCSprite implements DisplayableSprite, MovableSprite {
-
+	
 }
