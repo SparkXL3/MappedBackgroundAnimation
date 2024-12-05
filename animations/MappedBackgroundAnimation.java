67d7d175e06341709e68f11a20a4c8132bc1e329
gjwehnes <gjwehnes@E829WL240006.org.cbe.ab.ca>
2024-12-04 18:12:42 -0700

    updated to align with revisions summer 2024

--- animations/MappedBackgroundAnimation.java
+++ animations/MappedBackgroundAnimation.java
@@ -20,5 +20,32 @@ public class MappedBackgroundAnimation implements Animation {
 	public Universe getCurrentUniverse() {
 		return this.current;
 	}
+
+	@Override
+	public Universe switchUniverse(Object event) {
+		return null;
+	}
+
+	@Override
+	public boolean getUniverseSwitched() {
+		return false;
+	}
+
+	@Override
+	public void acknowledgeUniverseSwitched() {		
+	}
+
+	@Override
+	public boolean isComplete() {
+		return false;
+	}
+
+	@Override
+	public void setComplete(boolean complete) {
+	}
+
+	@Override
+	public void update(AnimationFrame frame, long actual_delta_time) {
+	}
 	
 }
